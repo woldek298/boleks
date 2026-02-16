e9ec3c71e9fa60bf61ba33d14fcd11796d3d082c
woldek298 <46133348+woldek298@users.noreply.github.com>
2026-02-16 17:35:37 +0100

    cuda: optimize s_sieve memory addressing and aliasing

--- xpm/cuda/sieve.cu
+++ xpm/cuda/sieve.cu
@@ -12,16 +12,17 @@ __constant__ uint32_t nps_all[] = { 2, 2, 3, 4, 5, 5, 5, 6, 6 }; // 256 threads
 #error "Unsupported LSIZELOG2 constant"
 #endif
 
-__global__ void sieve(uint32_t *gsieve_all,
-                      uint32_t* offset_all,
-                      uint2 *primes)
+__global__ void sieve(uint32_t *__restrict__ gsieve_all,
+                      const uint32_t *__restrict__ offset_all,
+                      const uint2 *__restrict__ primes)
 {
   __shared__ uint32_t sieve[SIZE];
   
   const uint32_t id = threadIdx.x;
   const uint32_t stripe = blockIdx.x;
   const uint32_t line = blockIdx.y; 
-  const uint32_t entry = SIZE*32*(stripe+STRIPES/2);
+  const uint32_t sieveBits = SIZE * 32;
+  const uint32_t entry = sieveBits * (stripe + STRIPES/2);
   const float fentry = entry;
   
   const uint32_t* offset = &offset_all[PCOUNT*line];
@@ -57,10 +58,11 @@ __global__ void sieve(uint32_t *gsieve_all,
 
     pos += __umul24(lpoff, prime);
 
+    const uint32_t step = __umul24(var, prime);
     uint4 vpos = {pos,
-                  pos + __umul24(var, prime),
-                  pos + __umul24(var*2, prime),
-                  pos + __umul24(var*3, prime)};
+                  pos + step,
+                  pos + step * 2,
+                  pos + step * 3};
 
     if (var*4 >= 32) {
       uint32_t *s1 = &sieve[vpos.x >> 5];
@@ -68,10 +70,10 @@ __global__ void sieve(uint32_t *gsieve_all,
       uint32_t *s3 = &sieve[vpos.z >> 5];
       uint32_t *s4 = &sieve[vpos.w >> 5];
       uint32_t *se = &sieve[SIZE];
-      uint32_t bit1 = orb << (vpos.x % 32);
-      uint32_t bit2 = orb << (vpos.y % 32);
-      uint32_t bit3 = orb << (vpos.z % 32);
-      uint32_t bit4 = orb << (vpos.w % 32);
+      uint32_t bit1 = orb << (vpos.x & 31);
+      uint32_t bit2 = orb << (vpos.y & 31);
+      uint32_t bit3 = orb << (vpos.z & 31);
+      uint32_t bit4 = orb << (vpos.w & 31);
       const uint32_t add = var*4*prime >> 5;
       while (s4 < se) {
         atomicOr(s1, bit1);
@@ -93,24 +95,24 @@ __global__ void sieve(uint32_t *gsieve_all,
     } else {
 
 
-    const uint32_t add = var*4*prime;
-    while (vpos.w < SIZE*32) {
-      atomicOr(&sieve[vpos.x >> 5], orb << (vpos.x%32));
-      atomicOr(&sieve[vpos.y >> 5], orb << (vpos.y%32));
-      atomicOr(&sieve[vpos.z >> 5], orb << (vpos.z%32));
-      atomicOr(&sieve[vpos.w >> 5], orb << (vpos.w%32));
+    const uint32_t add = step * 4;
+    while (vpos.w < sieveBits) {
+      atomicOr(&sieve[vpos.x >> 5], orb << (vpos.x & 31));
+      atomicOr(&sieve[vpos.y >> 5], orb << (vpos.y & 31));
+      atomicOr(&sieve[vpos.z >> 5], orb << (vpos.z & 31));
+      atomicOr(&sieve[vpos.w >> 5], orb << (vpos.w & 31));
       vpos.x += add;
       vpos.y += add;
       vpos.z += add;
       vpos.w += add;
     }
 
-    if (vpos.x < SIZE*32)
-      atomicOr(&sieve[vpos.x >> 5], orb << (vpos.x%32));
-    if (vpos.y < SIZE*32)
-      atomicOr(&sieve[vpos.y >> 5], orb << (vpos.y%32));
-    if (vpos.z < SIZE*32)
-      atomicOr(&sieve[vpos.z >> 5], orb << (vpos.z%32));
+    if (vpos.x < sieveBits)
+      atomicOr(&sieve[vpos.x >> 5], orb << (vpos.x & 31));
+    if (vpos.y < sieveBits)
+      atomicOr(&sieve[vpos.y >> 5], orb << (vpos.y & 31));
+    if (vpos.z < sieveBits)
+      atomicOr(&sieve[vpos.z >> 5], orb << (vpos.z & 31));
     }
   }
   
@@ -154,41 +156,41 @@ __global__ void sieve(uint32_t *gsieve_all,
         
       const uint32_t add = 2*prime;
 
-      while (vpos.y < SIZE*32) {
-        atomicOr(&sieve[vpos.x >> 5], 1u << (vpos.x%32));
-        atomicOr(&sieve[vpos.y >> 5], 1u << (vpos.y%32));
+      while (vpos.y < sieveBits) {
+        atomicOr(&sieve[vpos.x >> 5], 1u << (vpos.x & 31));
+        atomicOr(&sieve[vpos.y >> 5], 1u << (vpos.y & 31));
         vpos.x += add;
         vpos.y += add;
       }
         
-      if (vpos.x < SIZE*32)
-        atomicOr(&sieve[vpos.x >> 5], 1u << (vpos.x % 32));
+      if (vpos.x < sieveBits)
+        atomicOr(&sieve[vpos.x >> 5], 1u << (vpos.x & 31));
     } else if (ip < SIEVERANGE2) {
       if(index < SIZE){
-        atomicOr(&sieve[index], 1u << (pos%32));
+        atomicOr(&sieve[index], 1u << (pos & 31));
         pos += prime;
         index = pos >> 5;
         if(index < SIZE){
-          atomicOr(&sieve[index], 1u << (pos%32));
+          atomicOr(&sieve[index], 1u << (pos & 31));
           pos += prime;
           index = pos >> 5;
           if(index < SIZE){
-            atomicOr(&sieve[index], 1u << (pos%32));
+            atomicOr(&sieve[index], 1u << (pos & 31));
           }
         }
       }
     } else if(ip < SIEVERANGE3) {
       if(index < SIZE){
-        atomicOr(&sieve[index], 1u << (pos%32));
+        atomicOr(&sieve[index], 1u << (pos & 31));
         pos += prime;
         index = pos >> 5;
         if(index < SIZE){
-          atomicOr(&sieve[index], 1u << (pos%32));
+          atomicOr(&sieve[index], 1u << (pos & 31));
         }
       }
     } else {
       if(index < SIZE){
-        atomicOr(&sieve[index], 1u << (pos%32));
+        atomicOr(&sieve[index], 1u << (pos & 31));
       }
     }
     
@@ -203,7 +205,7 @@ __global__ void sieve(uint32_t *gsieve_all,
     }
     
     lpos++;
-    lpos = lpos % NLIFO;
+    lpos = lpos & (NLIFO - 1);
   }
 
 #pragma unroll
@@ -221,7 +223,7 @@ __global__ void sieve(uint32_t *gsieve_all,
 
     uint32_t index = pos >> 5;
     if(index < SIZE)
-      atomicOr(&sieve[index], 1u << (pos%32));
+      atomicOr(&sieve[index], 1u << (pos & 31));
 
     if(ip+NLIFO < SCOUNT/LSIZE){
       pprimes += LSIZE;
@@ -234,7 +236,7 @@ __global__ void sieve(uint32_t *gsieve_all,
     }
 
     lpos++;
-    lpos = lpos % NLIFO;
+    lpos = lpos & (NLIFO - 1);
   }
 
   __syncthreads();
@@ -243,21 +245,40 @@ __global__ void sieve(uint32_t *gsieve_all,
     gsieve[i] = sieve[i];
 }
 
-__global__ void s_sieve(const uint32_t *gsieve1,
-                        const uint32_t* gsieve2,
-                        fermat_t *found320,
-                        fermat_t *found352,
-                        uint32_t *fcount,
+
+__device__ __forceinline__ void appendFoundWarp(bool is320,
+                                                const fermat_t &info,
+                                                uint32_t *fcount,
+                                                fermat_t *found320,
+                                                fermat_t *found352)
+{
+  const uint32_t addr = atomicAdd(&fcount[is320 ? 0 : 1], 1);
+  if (is320)
+    found320[addr] = info;
+  else
+    found352[addr] = info;
+}
+
+__global__ void s_sieve(const uint32_t *__restrict__ gsieve1,
+                        const uint32_t *__restrict__ gsieve2,
+                        fermat_t *__restrict__ found320,
+                        fermat_t *__restrict__ found352,
+                        uint32_t *__restrict__ fcount,
                         uint32_t hashid,
                         uint32_t hashSize,
                         uint32_t depth)
 {
   const uint32_t id = blockIdx.x * blockDim.x + threadIdx.x;
 
+  const uint32_t sieveSpanWords = SIZE * STRIPES / 2;
+
   uint32_t tmp1[WIDTH];
+  const uint32_t *g1 = gsieve1 + id;
 #pragma unroll
-  for (int i = 0; i < WIDTH; ++i)
-    tmp1[i] = gsieve1[SIZE*STRIPES/2*i + id];
+  for (int i = 0; i < WIDTH; ++i) {
+    tmp1[i] = *g1;
+    g1 += sieveSpanWords;
+  }
 
 #pragma unroll
   for (int start = 0; start <= WIDTH-TARGET; ++start){
@@ -271,8 +292,6 @@ __global__ void s_sieve(const uint32_t *gsieve1,
       unsigned bit = 31-__clz(~mask);
       unsigned multiplier = bit + id*32 + SIZE*32*STRIPES/2;  // mad24(id, 32u, (unsigned)bit) + SIZE*32*STRIPES/2;
       unsigned maxSize = hashSize + (32-__clz(multiplier)) + start + depth;
-      const uint32_t addr = atomicAdd(&fcount[(maxSize <= 320) ? 0 : 1], 1);
-      fermat_t *found = (maxSize <= 320) ? found320 : found352;
 
       fermat_t info;
       info.index = multiplier;
@@ -280,14 +299,17 @@ __global__ void s_sieve(const uint32_t *gsieve1,
       info.chainpos = 0;
       info.type = 0;
       info.hashid = hashid;
-      found[addr] = info;
+      appendFoundWarp(maxSize <= 320, info, fcount, found320, found352);
     }
   }
 
   uint32_t tmp2[WIDTH];
+  const uint32_t *g2 = gsieve2 + id;
 #pragma unroll
-  for (int i = 0; i < WIDTH; ++i)
-    tmp2[i] = gsieve2[SIZE*STRIPES/2*i + id];
+  for (int i = 0; i < WIDTH; ++i) {
+    tmp2[i] = *g2;
+    g2 += sieveSpanWords;
+  }
 
 #pragma unroll
   for (int start = 0; start <= WIDTH-TARGET; ++start){
@@ -300,8 +322,6 @@ __global__ void s_sieve(const uint32_t *gsieve1,
       unsigned bit = 31-__clz(~mask);
       unsigned multiplier = bit + id*32 + SIZE*32*STRIPES/2;  // mad24(id, 32u, (unsigned)bit) + SIZE*32*STRIPES/2;
       unsigned maxSize = hashSize + (32-__clz(multiplier)) + start + depth;
-      const uint32_t addr = atomicAdd(&fcount[(maxSize <= 320) ? 0 : 1], 1);
-      fermat_t *found = (maxSize <= 320) ? found320 : found352;
 
       fermat_t info;
       info.index = multiplier;
@@ -309,7 +329,7 @@ __global__ void s_sieve(const uint32_t *gsieve1,
       info.chainpos = 0;
       info.type = 1;
       info.hashid = hashid;
-      found[addr] = info;
+      appendFoundWarp(maxSize <= 320, info, fcount, found320, found352);
     }
   }
 
@@ -331,8 +351,6 @@ __global__ void s_sieve(const uint32_t *gsieve1,
       unsigned bit = 31-__clz(~mask);
       unsigned multiplier = bit + id*32 + SIZE*32*STRIPES/2;  // mad24(id, 32u, (unsigned)bit) + SIZE*32*STRIPES/2;
       unsigned maxSize = hashSize + (32-__clz(multiplier)) + start + (depth/2) + (depth&1);
-      const uint32_t addr = atomicAdd(&fcount[(maxSize <= 320) ? 0 : 1], 1);
-      fermat_t *found = (maxSize <= 320) ? found320 : found352;
 
       fermat_t info;
       info.index = multiplier;
@@ -340,7 +358,7 @@ __global__ void s_sieve(const uint32_t *gsieve1,
       info.chainpos = 0;
       info.type = 2;
       info.hashid = hashid;
-      found[addr] = info;
+      appendFoundWarp(maxSize <= 320, info, fcount, found320, found352);
     }
   }
 }
